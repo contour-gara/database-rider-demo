93226415e61f3dcd9f88da174af4a0b723195839
Contour gara <65527071+contour-gara@users.noreply.github.com>
2023-10-20 00:40:53 +0900

    first commit

---
+++ src/main/java/databaseriderdemo/DatabaseRiderDemoApplication.java
@@ -0,0 +1,13 @@
+package databaseriderdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseRiderDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseRiderDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/databaseriderdemo/DatabaseRiderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package databaseriderdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseRiderDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
